ff926326ff459a51d7dc80d1024c1b98eb428f74
squarebe <squarebe03@gmail.com>
2015-09-02 10:59:52 +0100

    third test passing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,5 +5,14 @@ class RestaurantsController < ApplicationController
 
   def new
   end
-  
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
