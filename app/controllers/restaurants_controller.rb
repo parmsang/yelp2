0bd3d90d0fe5c52137038fcb4684a95fffbbbeb5
squarebe <squarebe03@gmail.com>
2015-09-02 13:00:21 +0100

    we can edit restaurants, showing restaurants fixed

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,17 +7,28 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+  
   def create
     Restaurant.create(restaurant_params)
     redirect_to '/restaurants'
   end
 
-  def restaurant_params
-    params.require(:restaurant).permit(:name)
-  end
-
   def show
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to '/restaurants'
   end
 
 end
