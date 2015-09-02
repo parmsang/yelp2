9e88940c38dc128a5708be7c066f51ae49521c38
squarebe <squarebe03@gmail.com>
2015-09-02 15:29:18 +0100

    unit test for validation

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,4 +2,10 @@ require 'spec_helper'
 
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
+
+  it 'is not valid with a name of less than three characters' do
+    restaurant = Restaurant.new(name: "kf")
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
 end
