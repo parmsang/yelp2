1f78c9685afed867da3788df6f5980dd9d516628
Ajitsy <sanghera.p@gmail.com>
2015-09-03 11:22:33 +0100

    intergrated user sign up vis gem devise all tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'restaurants' => 'restaurants#index'
   resources :restaurants do
     resources :reviews
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root to: "restaurants#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
