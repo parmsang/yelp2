9515faa8cbf2a383865f3bdd709e48b721619f5a
squarebe <squarebe03@gmail.com>
2015-09-05 12:50:31 +0100

    average review score showing

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,10 @@ class Restaurant < ActiveRecord::Base
   belongs_to :user
   has_many :reviews, dependent: :destroy
   validates :name, length: {minimum: 3}, uniqueness: true
+
+  def average_rating
+    return 'N/A' if reviews.none?
+    reviews.inject(0) {|memo, review| memo + review.rating} / reviews.count
+  end
+
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -13,6 +13,7 @@ feature 'reviewing' do
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
   end
+
   before do
     visit('/')
     click_link('Sign up')
@@ -21,6 +22,19 @@ feature 'reviewing' do
     fill_in('Password confirmation', with: 'testtest')
     click_button('Sign up')
   end
+
+  scenario 'displays an average rating for all reviews' do
+    leave_review('So so', '3')
+    click_link 'Sign out'
+    click_link('Sign up')
+    fill_in('Email', with: 'test2@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
+    leave_review('Great', '5')
+    expect(page).to have_content('Average rating: 4')
+  end
+
   scenario 'a user can only review a restaurant once' do
     click_link 'Review KFC'
     fill_in "Thoughts", with: "so so"
@@ -31,4 +45,13 @@ feature 'reviewing' do
     expect(page).to have_content 'You have already reviewed this restaurant'
     expect(page).not_to have_content 'Meh'
   end
+
+  def leave_review(thoughts, rating)
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: thoughts
+    select rating, from: 'Rating'
+    click_button 'Leave Review'
+  end
+
 end
