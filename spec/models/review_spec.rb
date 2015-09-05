c9b713606618fa0a94e334b524054fda66848bc6
Ajitsy <sanghera.p@gmail.com>
2015-09-05 13:40:19 +0100

    can see rating points as stars

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 describe Review, :type => :model do
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :restaurant }
-  
+
   it 'is invalid if the rating is more than 5' do
     review=Review.new(rating: 10)
     expect(review).to have(1).error_on(:rating)
   end
+
 end
